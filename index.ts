0dd1e58ff948877f688851b2217ac40a4795ff81
Rori <rbmasilo@gmail.com>
2024-06-11 07:56:04 +0200

    Refactor index.ts: add new variables and imports, modify reviews array

--- index.ts
+++ index.ts
@@ -1,26 +1,38 @@
 // Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to 
+// Write a function that will only accept numbers and attend to
 // all TypeScript weakness flags.
 // : number
-const reviewTotalDisplay = document.querySelector('#reviews')
 
-const reviews = [
-    {
-        name: 'Sheia',
-        stars: 5,
-        loyaltyUser: true,
-        date: '01-04-2021'
-    },
-    {
-        name: 'Andrzej',
-        stars: 3,
-        loyaltyUser: false,
-        date: '28-03-2021'
-    },
-    {
-        name: 'Omar',
-        stars: 4,
-        loyaltyUser: true,
-        date: '27-03-2021'
-    },
-]
+const reviewTotalDisplay = document.querySelector("#reviews");
+const propertyContainer = document.querySelector(".properties");
+const footer = document.querySelector(".footer");
+
+import { showReviewTotal, populateUser } from "./utils";
+let isOpen: boolean;
+
+//Reviews
+const reviews: {
+  name: string;
+  stars: number;
+  loyaltyUser: boolean;
+  date: string;
+}[] = [
+  {
+    name: "Sheia",
+    stars: 5,
+    loyaltyUser: true,
+    date: "01-04-2021",
+  },
+  {
+    name: "Andrzej",
+    stars: 3,
+    loyaltyUser: false,
+    date: "28-03-2021",
+  },
+  {
+    name: "Omar",
+    stars: 4,
+    loyaltyUser: true,
+    date: "27-03-2021",
+  },
+];
